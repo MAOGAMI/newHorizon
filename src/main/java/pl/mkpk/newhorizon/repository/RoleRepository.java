c9ec1e4c63929bb6eb114991690dcd2ee81ba8a8
Mateusz Dubiniewicz <28206886+MAOGAMI@users.noreply.github.com>
2019-03-16 12:05:36 +0100

    repository for user and role

---
+++ src/main/java/pl/mkpk/newhorizon/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package pl.mkpk.newhorizon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.mkpk.newhorizon.model.Role;
+
+@Repository()
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+}
