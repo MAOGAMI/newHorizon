61e5ebbaf2ce4c7a7d591825bdd9be68441076aa
Mateusz Dubiniewicz <28206886+MAOGAMI@users.noreply.github.com>
2019-03-16 11:02:57 +0100

    initial commit

---
+++ src/main/java/pl/mkpk/newhorizon/NewhorizonApplication.java
@@ -0,0 +1,13 @@
+package pl.mkpk.newhorizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewhorizonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewhorizonApplication.class, args);
+    }
+
+}
