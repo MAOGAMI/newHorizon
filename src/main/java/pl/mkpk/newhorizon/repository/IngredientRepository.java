a3bead98c825c4afc1389fbe297a1d6bde329c60
Mao <28206886+MAOGAMI@users.noreply.github.com>
2019-03-18 21:05:01 +0100

    addet target to gitignore, added @repository to ingredientsrepo

--- src/main/java/pl/mkpk/newhorizon/repository/IngredientRepository.java
+++ src/main/java/pl/mkpk/newhorizon/repository/IngredientRepository.java
@@ -1,7 +1,9 @@
 package pl.mkpk.newhorizon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.mkpk.newhorizon.cookbook.Ingredient;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 }
