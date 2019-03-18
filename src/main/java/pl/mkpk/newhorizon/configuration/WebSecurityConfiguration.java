bdd9424e0fcce43e81f6fdc7a05f6de49c42fe04
Mao <28206886+MAOGAMI@users.noreply.github.com>
2019-03-18 23:17:54 +0100

    new login & register security old security config commented out

---
+++ src/main/java/pl/mkpk/newhorizon/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,70 @@
+package pl.mkpk.newhorizon.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .authorizeRequests()
+//                .antMatchers("/changePassword")
+//                .hasAnyAuthority("ROLE_USER", "ROLE_ADMIN", "ROLE_DIETICAN")
+//                .anyRequest()
+//                .permitAll()
+//                .and()
+//                .csrf()
+//                .disable()
+//                .formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .loginProcessingUrl("/login-process")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/errorLogout")
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutSuccessUrl("/");
+        httpSecurity.httpBasic().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .usersByUsernameQuery(
+                        "SELECT u.email, u.password, u.active FROM user u" +
+                                "WHERE u.email = ?")
+                .authoritiesByUsernameQuery(
+                        "SELECT u.email, r.role_name FROM user u" +
+                                "JOIN user_role ur ON ur.user_id = u.id" +
+                                "JOIN role r ON ur.roles_id = r.id" +
+                                "WHERE u.email = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/pl/mkpk/newhorizon/controller/LoginController.java
+++ src/main/java/pl/mkpk/newhorizon/controller/LoginController.java
@@ -1,14 +1,12 @@
 package pl.mkpk.newhorizon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.mkpk.newhorizon.model.User;
 import pl.mkpk.newhorizon.service.UserService;
@@ -17,54 +15,36 @@ import javax.validation.Valid;
 
 @Controller
 public class LoginController {
-    @Autowired
-    UserService userService;
+    private UserService userService;
 
-    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    @Autowired
+    public LoginController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/login.html")
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return modelAndView;
     }
 
-    @GetMapping("/registration")
-    public String registration(Model model) {
+    // obsługa żądzania przekierowania na stronę formularza
+    @GetMapping("/register.html")
+    public String register(Model model){
         User user = new User();
+        // dodajemy atrybut dla obiektu klasy Model ("nazwa stosowanana w html", nazwa bjektu Java)
         model.addAttribute("user", user);
-        return "registration";
+        return "register";  // przekierowanie na widok formularza rejestracji (html)
     }
-
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView modelAndView = new ModelAndView();
-        User userExists = userService.findUserByEmail(user.getEmail());
-        if (userExists != null) {
-            bindingResult
-                    .rejectValue("email", "error.user",
-                            "There is already a user registered with the email provided");
+    // obsługa żądzania przekazania obiektu z formularz metodą post
+    @PostMapping("/register.html")
+    public String register(@ModelAttribute @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "register";
         }
-        if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("registration");
-        } else {
-            userService.saveUser(user);
-            System.out.println(user);
-            modelAndView.addObject("successMessage", "User has been registered successfully");
-            modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration");
-
-        }
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/templates/admin/home", method = RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("AUTH ma wartosc: " + auth);
-        User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage", "Content Available Only for Users with Admin Role");
-        modelAndView.setViewName("admin/home");
-        return modelAndView;
+        // zapis do DB przez serwis użytkownika
+        userService.registerUser(user);
+        return "register";
     }
 }
