fbb6ec5fba890f018348eb93316841dfdcba1287
Mao <28206886+MAOGAMI@users.noreply.github.com>
2019-03-22 19:54:30 +0100

    login and register mappings w.i.p

--- src/main/java/pl/mkpk/newhorizon/controller/LoginController.java
+++ src/main/java/pl/mkpk/newhorizon/controller/LoginController.java
@@ -9,20 +9,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.mkpk.newhorizon.model.User;
+import pl.mkpk.newhorizon.repository.RoleRepository;
 import pl.mkpk.newhorizon.service.UserService;
 
 import javax.validation.Valid;
+import java.util.Arrays;
 
 @Controller
 public class LoginController {
     private UserService userService;
+    private RoleRepository roleRepository;
 
     @Autowired
-    public LoginController(UserService userService){
+    public LoginController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/login.html")
+    @GetMapping(value = {"/", "/login"})
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
@@ -30,21 +33,24 @@ public class LoginController {
     }
 
     // obsługa żądzania przekierowania na stronę formularza
-    @GetMapping("/register.html")
-    public String register(Model model){
+    @GetMapping("/register")
+    public String registerUser(Model model) {
         User user = new User();
         // dodajemy atrybut dla obiektu klasy Model ("nazwa stosowanana w html", nazwa bjektu Java)
         model.addAttribute("user", user);
         return "register";  // przekierowanie na widok formularza rejestracji (html)
     }
+
     // obsługa żądzania przekazania obiektu z formularz metodą post
-    @PostMapping("/register.html")
-    public String register(@ModelAttribute @Valid User user, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "register";
         }
         // zapis do DB przez serwis użytkownika
+        user.setActive(1);
+        user.setRoles(Arrays.asList(roleRepository.findByName("USER")));
         userService.registerUser(user);
-        return "register";
+        return "redirect:login";
     }
 }
