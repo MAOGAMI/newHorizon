9dd2bbb31340e0ff60d4e1ce42e116cf71717525
Runer <runer53248@gmail.com>
2019-03-23 12:09:25 +0100

    Some change.

--- src/main/java/pl/mkpk/newhorizon/cookbook/RunAtStart.java
+++ src/main/java/pl/mkpk/newhorizon/cookbook/RunAtStart.java
@@ -21,85 +21,32 @@ public class RunAtStart {
 
     @PostConstruct
     public void GenerateIngredients(){
-        IngredientsBase ingredientsBase= new IngredientsBase();
-        /*ingredientsBase.addIngredient(new Ingredient(
+        //IngredientsBase ingredientsBase= new IngredientsBase();
+
+
+        Ingredient newIngredient= new Ingredient(
                 "szklanka mąki pszennej",
                 new BigDecimal(250),
                 StackType.GlASS,
                 true,
-                new BigDecimal(180))
-        );
-        ingredientsBase.addIngredient(new Ingredient(
-                "łyżeczka proszku do pieczenia",
-                new BigDecimal(5),
-                StackType.TEASPOON,
-                false,
-                new BigDecimal(65))
-        );
-        ingredientsBase.addIngredient(new Ingredient(
-                "szczypta soli",
-                new BigDecimal(1),
-                StackType.PINCH,
-                false,
-                new BigDecimal(18))
-        );
-        ingredientsBase.addIngredient(new Ingredient(
-                "łyżka cukru wanilinowego",
-                new BigDecimal(15),
-                StackType.SPOON,
-                false,
-                new BigDecimal(65))
-        );
-        ingredientsBase.addIngredient(new Ingredient(
-                "szklanka oleju roślinnego",
-                new BigDecimal(250),
-                StackType.GlASS,
-                false,
-                new BigDecimal(180))
-        );
-        ingredientsBase.addIngredient(new Ingredient(
-                "szklanka mleka",
-                new BigDecimal(250),
-                StackType.GlASS,
-                true,
-                new BigDecimal(180))
-        );
-        Map<String, Ingredient> selects = ingredientsBase.ingredientMap;
+                new BigDecimal(180));
 
-        for(Map.Entry<String, Ingredient> entry : selects.entrySet()) {
-            String key = entry.getKey();
-            Ingredient value = entry.getValue();
-
-            ingredientRepository.save(value);
-        }*/
-
-        /*{
-            List<Ingredient> ingredient=ingredientRepository.findByName("Mąka");
-            System.out.println(ingredient);
-        }
+        ingredientRepository.save(newIngredient);
 
         List<Ingredient> ingredientList=ingredientRepository.findAll();
 
         for (Ingredient ingredient:ingredientList) {
-            ingredientsBase.addIngredient(ingredient);
+            System.out.println(ingredient);
         }
 
-        System.out.println(ingredientsBase);*/
-
-        /*CookingRecipe recipe= new CookingRecipe.RecipeBuilder(ingredientRepository)
-                .setName("GOFRY DOMOWE - Lekkie i chrupiące")
-                .addIngredients( "szklanka mąki pszennej",new BigDecimal(1.5))
-                .addIngredients( "łyżeczka proszku do pieczenia",new BigDecimal(1.5))
-                .addIngredients( "szczypta soli",new BigDecimal(1))
-                .addIngredients( "łyżka cukru wanilinowego",new BigDecimal(1))
-                .addIngredients( "Jajka",new BigDecimal(2))
-                .addIngredients( "szklanka oleju roślinnego",new BigDecimal(0.5))
-                .addIngredients( "szklanka mleka",new BigDecimal(1.33))
-                .addPreparationStep("Mąkę wsypać do miski, dodać proszek do pieczenia, sól, cukier, cukier wanilinowy. Wszystko wymieszać a następnie dodać jajka, olej roślinny oraz mleko. ...",new BigDecimal(20))
+        CookingRecipe recipe= new CookingRecipe.RecipeBuilder(ingredientRepository)
+                .setName("Jajko z jajkiem na jajku")
+                .addIngredients( "Jajka",new BigDecimal(3))
+                .addPreparationStep("Dodaj jajko do jajka i umieść na jaku.",new BigDecimal(60))
                 .build();
 
         cookingRecipeRepository.save(recipe);
 
-        System.out.println(recipe.toString());*/
+        System.out.println(recipe.toString());
     }
 }
